8a42f6e0b4334f9febe90c2260854aebe6b6efff
darshan009 <darshansapaliga009@gmail.com>
2015-11-20 19:06:18 +0530

    Final code

--- js/main.js
+++ js/main.js
@@ -5,29 +5,28 @@ var tile = function(row, column, value) {
     this.value = value;
 };
 
-console.log("tilepart??");
-var board = function() 
+var player = function(name, score)
 {
+    this.name = name;
+    this.score = score;
+}
+
+var players= new player(prompt("Enter your name?") , 0);
+
+var board = function() {
     this.tiles = [];
 
-
-    this.init = function() 
-    {
-
-
+    this.init = function() {
         for (i = 0; i < 7; i++) {
             for (j = 0; j < 7; j++) {
 
                 var tileObject = new tile(i, j, '');
                 this.tiles.push(tileObject);
-                console.log("tilepart??");
             }
         }
-
     };
     //draw tiles
-    this.drawBoard = function() 
-    {
+    this.drawBoard = function() {
 
         var boardDiv = document.createElement('div');
         boardDiv.className = 'board';
@@ -40,68 +39,42 @@ var board = function()
             rowDiv[i] = document.createElement('div');
             rowDiv[i].className = 'row';
         }
-
+//
         //for all 49 tiles
-        for (var i = 0; i < this.tiles.length; i++) {
+        for (i = 0; i < this.tiles.length; i++) {
             tileDiv = document.createElement('span');
             tileDiv.className = 'tile';
-            tileDiv.setAttribute('id', 'tile[' + i + ']');
+            tileDiv.setAttribute('id', 'tile['+i+']');
             tileDiv.setAttribute('data-row', this.tiles[i].row);
-            tileDiv.setAttribute('data-col', this.tiles[i].col);
+            tileDiv.setAttribute('data-col', this.tiles[i].column);
+            tileDiv.setAttribute('data-value', this.tiles[i].value);
 
-           
-           // tileDiv.innerHTML = 'B';
             rowDiv[row].appendChild(tileDiv);
             if (i == 6 || i == 13 || i == 20 || i == 27 || i == 34 || i == 41 || i == 49)
                 row++;
 
-
-             // creating array for nearby tiles
-            this.near = [];
-            function nearValues()
-            {
-            	var mineCount = 0;
-            
-	           
-	            if(this.tiles[i+7].value == 'B' && this.tiles[i+1].value == 'B' && this.tiles[i-1].value == 'B' && this.tiles[i-7].value == 'B')
-	            {
-	          		//this.tiles[i].value = '4';
-	          		
-
-	            }
-	             /*
-	            if(this.tiles[i+7].value == 'B' && this.tiles[i+1].value == 'B' && this.tiles[i-1].value == 'B' && this.tiles[i-7].value == 'B')
-	            {
-	            this.tiles[i].value = '3';
-	            }
-	            if(this.tiles[i+7].value == 'B' && this.tiles[i+1].value == 'B' && this.tiles[i-1].value == 'B' && this.tiles[i-7].value == 'B')
-	            {
-	            this.tiles[i].value = '2';
-	            }
-	            if(this.tiles[i+7].value == 'B' && this.tiles[i+1].value == 'B' && this.tiles[i-1].value == 'B' && this.tiles[i-7].value == 'B')
-	            {
-	            this.tiles[i].value = '1';
-	            }
-	            */
-
-      		}
+            player.score = 0;
             //onclick event
-            tileDiv.addEventListener('click', function(event) {
-            	var showTiles = document.getElementsByClassName("show");
+            tileDiv.addEventListener('click', function(event) 
+            {
+                this.style.color = 'black';
+                if (this.innerHTML == 'B') 
+                {
+                    alert("Game Over");
 
-            	if(this.tiles[i].value == 'B')
-            	{
-            		alert("Game Over");
-            		//show all
-            	}
-				this.querySelector(".number").classList.add("show");
-				this.querySelector(".suit").classList.add("show");
+                }
+                if (this.innerHTML !== 'B') 
+                {
+                    
+                    players.score += parseInt(10);
+                    document.getElementsByClassName('score')[0].innerHTML = players.score;
+                }
+                if(players.score == 390)
+                    alert("You Win!");
 
             });
 
-            console.log("drawpart");
         }
-
 
         for (i = 0; i < 7; i++) {
             //do
@@ -111,52 +84,120 @@ var board = function()
         //do
         document.body.appendChild(boardDiv);
 
-    };//end drawBoard
+    }; //end drawBoard
+
+
 
     //creating random bombs
-    this.bombs = function() 
-    {
-    	
-    	bombDiv = document.createElement('div');
-        bombDiv.className = 'card';
-        
-        for (var k = 0; k < 10; k++) 
-        {
-            var rand = function(max) 
+    this.randArr = [];
+
+    this.bombsDraw = function() {
+
+        //ADDING 0 EVERYWHERE
+        for(var i=0;i<this.tiles.length;i++)
             {
-              return (Math.floor(Math.random() * max) + 1);
-            };
-            tileDiv.innerHTML = 'B';
-	        this.tiles[rand(49)].value = tileDiv;
-            console.log(this.tiles[rand(49)].value);
-            console.log(rand(49));
-         
-            document.body.appendChild(tileDiv);
+                document.getElementById('tile['+(i)+']').innerHTML = 0;
+            }
+        //adding bombs
+        for (var i = 0; i < 10; i++) {
+            var randomNo = rand(48);
+            document.getElementById('tile['+(randomNo)+']').innerHTML = 'B';
+            this.randArr.push(randomNo);
         }
-			
-        /* avoiding duplicates bomb position
-			this.bombs = [[]];
-			while (this.bombs.length < 7)
-			{
-				var r = Math.floor(Math.random() * 7) + 1;
-				var c = Math.floor(Math.random() * 7) + 1;
-				for(i = 0; i <this.bombs.length; i++)
-				{
-				
-				}
-			}
-
-        */
-
-    };//end bombs
-     	
-
-};//end board
 
 
 
+        for(var i=0;i<49;i++)
+        {
+            if(document.getElementById('tile['+i+']').innerHTML == 'B')
+            {
+                 var nearBy = [i+1,i+7,i+6,i+8,i-1,i-7,i-6,i-8];
+                 var j = 0;
+                 while(j < nearBy.length)
+                 {
+                    if(nearBy[j] > -1 && nearBy[j] < 49)
+                    {
+                        var nearByValue = document.getElementById('tile['+(nearBy[j])+']').innerHTML;
+                        if(nearByValue !== 'B')
+                        {
+                            nearByValue = parseInt(nearByValue);
+                            nearByValue ++;
+                            document.getElementById('tile['+nearBy[j]+']').innerHTML = nearByValue;
+                        }
+                    }
+                    j++;
+                 }
+            }
+        }
 
+
+
+
+      //  console.log(randomNo);
+       
+    }; //end bombs
+
+
+}; //end board
+
+  //scores
+  scoreDiv = document.createElement('div');
+  scoreDiv.className ='scores';
+  scoreDiv.innerHTML='<span id="players" class="player"><p> Name:&nbsp&nbsp&nbsp ' + players.name +'<br/><b class="score"><p> Score:&nbsp&nbsp&nbsp</p>'+players.score+'';
+  document.body.appendChild(scoreDiv);
+
+
+
+function rand(max) {
+    return (Math.floor(Math.random() * max) + 1);
+}
+
+//calling the main function
 var myBoard = new board();
 myBoard.init();
 myBoard.drawBoard();
-myBoard.bombs();
+myBoard.bombsDraw();
+
+
+
+
+/*
+
+for(i=0;i<this.tiles.length;i++)
+            {
+
+                for(j=0;j<10;j++)
+                {
+                 //   if(this.tiles[i] !== this.randArr[j])
+                      
+                    if(this.tiles[i] === this.randArr[j])
+                    {
+                          document.getElementById('tile['+ (i+1) +']').innerHTML += 1;
+                    }
+                }   
+
+            }
+
+
+                loop->49;i
+{
+ 
+  var ngCell =[i-1,i+1,i+7.....]
+  var j =0
+  while(j<ngCell.length)
+  {
+    var ngCellValue = document.getElementById("tile["+ngCell[j]+"]").innerHTML;
+    if(!ngCellValue==B)
+    {
+      ngCellValue = parseInt(ngCellValue);
+      if(ngCell>-1)
+      {
+        ngCellValue++;
+        document.getElementById("tile["+ngCell[j]+"]").innerHTML = ngCellValue;      
+ 
+      }
+    }
+ 
+    j++
+  }
+*/
